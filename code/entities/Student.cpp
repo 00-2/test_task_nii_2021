3fc21ef86f68e6d32f7533e165376ccfad2557fa
00-2 <9x02@mail.ru>
2021-12-03 21:00:24 +0300

    change structure of student, add getter for id

--- code/entities/Student.cpp
+++ code/entities/Student.cpp
@@ -2,12 +2,22 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+
+
+Student::Student(std::string first_name,std::string last_name,tm date)
+{
+    this->first_name = first_name;
+    this->last_name = last_name;
+    this->date = date;    
+}
+Student::~Student(){}
+
+
 bool Student::operator==(const Student &ref) const{
     const Student *o = dynamic_cast<const Student*>(&ref);
     if (o==NULL) return false;
     if (o->first_name == this->first_name &&
         o->last_name == this->last_name &&
-        o->surname == this->surname && 
         o->date.tm_year == this->date.tm_year &&
         o->date.tm_mon == this->date.tm_mon &&
         o->date.tm_mday == this->date.tm_mday
@@ -17,7 +27,7 @@ bool Student::operator==(const Student &ref) const{
 std::istream& operator>>(std::istream& is, Student & ref){
     std::string s_date;
     unsigned int number;
-    std::cin >> number >> ref.first_name >> ref.last_name >> ref.surname >> s_date;
+    is >> number >> ref.first_name >> ref.last_name >> s_date;
     std::stringstream ss(s_date);
     char ch;
     ss >> ref.date.tm_mday >> ch >> ref.date.tm_mon >> ch >> ref.date.tm_year;
@@ -25,19 +35,9 @@ std::istream& operator>>(std::istream& is, Student & ref){
 }
 
 std::ostream& operator<<(std::ostream& os, Student & ref){
-    std::cout << ref.id << " " << ref.first_name << " " << ref.last_name << " " << ref.surname << " " << ref.date.tm_mday<<"."<<ref.date.tm_mon<<"."<<ref.date.tm_year;
+    std::cout << ref.id << " " << ref.first_name << " " << ref.last_name << " " << ref.date.tm_mday<<"."<<ref.date.tm_mon<<"."<<ref.date.tm_year;
     return os;
 }
-
-Student::Student(std::string first_name,std::string last_name,std::string surname, tm date)
-{
-    this->first_name = first_name;
-    this->last_name = last_name;
-    this->surname = surname;
-    this->date = date;    
-}
-Student::~Student(){}
-
 std::string Student::get_first_name() const{
     return this->first_name;
 }
@@ -45,6 +45,6 @@ std::string Student::get_first_name() const{
 std::string Student::get_last_name() const{
     return this->last_name;
 }
-std::string Student::get_surname() const {
-    return this->surname;
+tm Student::get_date() const{
+    return this->date;
 }
--- code/entities/Student.h
+++ code/entities/Student.h
@@ -5,17 +5,19 @@
 class Student:private Object
 {
     public:
-        Student(std::string first_name,std::string last_name,std::string surname, tm date);
+        Student(std::string first_name,std::string last_name,tm date);
+        Student(){};
         ~Student();
         bool operator==(const Student &ref) const;
         friend std::istream& operator>>(std::istream& is, Student &ref);
         friend std::ostream& operator<<(std::ostream& os, Student &ref);
+        void set_id(unsigned int id){ this->id = id;}
         std::string get_first_name () const;
         std::string get_last_name() const;
         std::string get_surname() const;
-
+        tm get_date() const;
     private:
-        std::string first_name, last_name, surname;
+        std::string first_name, last_name;
         tm date;
 };
 
@@ -23,6 +25,6 @@ class StudentHashFunction{
     public:
     size_t operator()(const Student& ref) const
     {
-        return (std::hash<std::string>{}(ref.get_first_name())) ^ (std::hash<std::string>{}(ref.get_last_name())) ^ (std::hash<std::string>{}(ref.get_surname()));
+        return (std::hash<std::string>{}(ref.get_first_name())) ^ (std::hash<std::string>{}(ref.get_last_name()));
     }
 };
