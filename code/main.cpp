04593c42b964d370cbe981c490330e05563ce611
00-2 <9x02@mail.ru>
2021-12-03 19:36:16 +0300

    adding files

--- code/main.cpp
+++ code/main.cpp
@@ -2,25 +2,44 @@
 #include <iostream>
 #include <unordered_set>
 #include <vector>
+#include <fstream>
 
-
+const std::string subdir = "../files/";
+const std::vector<std::string> name_of_files{"student_file_1.txt","student_file_2.txt"};
 
 int main(){
-    tm date;
-    std::string date_string = "30.9.1988";
-    strptime(date_string.c_str(), "%d.%m.%Y", &date);
+    //tm date;
+    //std::string date_string = "30.9.1988";
+    //strptime(date_string.c_str(), "%d.%m.%Y", &date);
     //printf("%d %d %d", date.tm_mday, date.tm_mon, date.tm_year);
-    Student s = Student("name", "sec", "sur", date);
-    Student s1 = Student("name", "test", "sur", date);
-    std::cin >> s;
+    //Student s = Student("name", "sec", "sur", date);
+    //Student s1 = Student("name", "test", "sur", date);
+    //std::cin >> s;
     // work with file
     std::unordered_set<Student,StudentHashFunction> set_from_files;
-    std::vector<std::string> name_of_files{"student_file_1.txt","student_file_2.txt"};
+    
     //
-    //ifstream file;
-    set_from_files.insert(s);
-    set_from_files.insert(s1);
-    for (Student elem: set_from_files) {
-        std::cout << elem << "\n";
+    //set_from_files.insert(s);
+    //set_from_files.insert(s1);
+    //for (Student elem: set_from_files) {
+    //    std::cout << elem << "\n";
+    //}
+
+
+    std::cout << "Subdirectory:" << subdir<< std::endl;
+    for (std::string name_of_file : name_of_files){
+        std::cout <<"Work with:" << name_of_file << std::endl;
+        std::ifstream fin;
+        fin.open(subdir+name_of_file);
+        if (!fin) {
+            std::cerr << "Error in opening the file:"<< name_of_file << std::endl;
+        }
+    
+    /*vector<People> people;
+    People temp;
+    while (fin >> temp.name >> temp.surname >> temp.years) {
+        people.push_back(temp);
+    }
+    */
     }
 }
